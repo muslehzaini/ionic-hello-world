d2cb4f5815f61dae964beab62fead0061c045302
Musleh_Zaini <muslehzaini@gmail.com>
2020-11-11 12:39:26 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,90 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+ count=0;
+ message=""
+
+  constructor(public alertController: AlertController,public toastController: ToastController,public actionSheetController: ActionSheetController) {}
+
+
+incrementPressed(){
+	this.count++
+}
+
+decrementPressed(){
+	
+	if (this.count == 0){
+		this.message="No can't be less than 0"
+	} 
+	else{
+		this.count--
+	}	
+}
+
+resetPressed(){
+	this.count=0;
+	this.message=""
+}
+
+async alertPressed(){
+	const alert = await this.alertController.create({
+      header: 'Hello World',
+      message: 'Welcome to my app',
+      buttons: ['Ok', 'Cancel']
+    });
+
+    await alert.present();
+}
+
+async toastPressed(){
+	const toast = await this.toastController.create({
+      message: 'Your settings have been saved.',
+      duration: 2000
+    });
+    toast.present();
+}
+
+async actionPressed() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 
 }
